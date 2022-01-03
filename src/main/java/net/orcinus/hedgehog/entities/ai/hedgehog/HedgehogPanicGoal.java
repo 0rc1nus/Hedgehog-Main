314bf33f57eeaad29751399305cb22b168aaf861
0rc1nus <mitchellctlui@gmail.com>
2022-01-03 08:41:26 +0800

    Renamed Method

--- src/main/java/net/orcinus/hedgehog/entities/ai/hedgehog/HedgehogPanicGoal.java
+++ src/main/java/net/orcinus/hedgehog/entities/ai/hedgehog/HedgehogPanicGoal.java
@@ -19,12 +19,12 @@ public class HedgehogPanicGoal extends Goal {
 
     @Override
     public boolean canUse() {
-        return this.hedgehog.isAlive() && this.isUserWithMilkAlive() != null;
+        return this.hedgehog.isAlive() && this.isUserScary() != null;
     }
 
     @Override
     public void tick() {
-        if (this.isUserWithMilkAlive() != null) {
+        if (this.isUserScary() != null) {
             this.hedgehog.setTarget(null);
             this.hedgehog.getNavigation().stop();
             this.hedgehog.setJumping(false);
@@ -32,7 +32,7 @@ public class HedgehogPanicGoal extends Goal {
         }
     }
 
-    private LivingEntity isUserWithMilkAlive() {
+    private LivingEntity isUserScary() {
         List<LivingEntity> possibles = Lists.newArrayList();
         List<LivingEntity> list = this.hedgehog.level.getEntitiesOfClass(LivingEntity.class, this.hedgehog.getBoundingBox().inflate(3.0D));
         for (LivingEntity livingEntity : list) {
